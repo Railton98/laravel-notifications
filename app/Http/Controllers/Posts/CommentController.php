9374d64bf9457fe60aad83dde41a1645e0512736
Railton98 <railtonleal98@gmail.com>
2018-10-01 14:56:30 -0300

    implementing email notifications

--- app/Http/Controllers/Posts/CommentController.php
+++ app/Http/Controllers/Posts/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Posts;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCommentFormRequest;
+use App\Notifications\PostCommented;
 
 class CommentController extends Controller
 {
@@ -37,6 +38,9 @@ class CommentController extends Controller
     public function store(StoreCommentFormRequest $request)
     {
         $comment = $request->user()->comments()->create($request->all());
+
+        $author = $comment->post->user;
+        $author->notify(new PostCommented($comment));
 
         return redirect()
                     ->route('posts.show', $comment->post_id)
