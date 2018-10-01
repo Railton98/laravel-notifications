39c53caa2a6cf675ac4e1ced9b2d0b65dee75c9c
Railton98 <railtonleal98@gmail.com>
2018-09-30 22:15:52 -0300

    Moving and Configuring the Namespace for the Model User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Comment;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Author Post
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
