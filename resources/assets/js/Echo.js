5f69def9173195027e22eb63ab6739e7597e8025
Railton98 <railtonleal98@gmail.com>
2018-10-03 15:54:58 -0300

    displaying new notification alerts with SweetAlert.js

--- resources/assets/js/Echo.js
+++ resources/assets/js/Echo.js
@@ -1,4 +1,5 @@
 import store from './vuex/store'
+import swall from 'sweetalert'
 
 const typesNotifications = {
     postCommented: 'App\\Notifications\\PostCommented',
@@ -7,8 +8,16 @@ const typesNotifications = {
 if (Laravel.user) {
     Echo.private(`App.Models.User.${Laravel.user}`)
             .notification(notification => {
-                if(notification.type == typesNotifications.postCommented) {
-                    store.commit('ADD_NOTIFICATION', notification)
+                if (Laravel.user == notification.data.comment.post.user.id) {
+                    if(notification.type == typesNotifications.postCommented) {
+                        swall(
+                            'Novo Comentário',
+                            `O post ${notification.data.comment.post.title} foi comentado por ${notification.data.comment.user.name}`,
+                            'info'
+                        )
+
+                        store.commit('ADD_NOTIFICATION', notification)
+                    }
                 }
             })
 }
